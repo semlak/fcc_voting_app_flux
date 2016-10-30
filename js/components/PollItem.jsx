8d6ae3785046823192dfb48adb6962442a0a57ff
Joseph <semlak@gmail.com>
2016-10-29 22:23:51 -0500

    converted PollItem, PollList, and UserList components to stateless functional components

--- js/components/PollItem.jsx
+++ js/components/PollItem.jsx
@@ -13,34 +13,70 @@ import ReactPropTypes from 'react/lib/ReactPropTypes';
 // import PollActionCreators from '../actions/ PollActionCreators';
 // import NavLink from './NavLink';
 
-var PollItem = React.createClass({
 
-	propTypes: {
-		poll: ReactPropTypes.object.isRequired,
-		handlePollSelect: React.PropTypes.func.isRequired
-	},
-
-	render: function() {
-		var poll = this.props.poll;
-		var author_label = 'Poll Author: ';
-		var question_label = 'Poll Question: ';
-		return (
-			<div className='poll well' id={'poll-' + poll.id} onClick={this.props.handlePollSelect.bind(null, poll.id)}>
-				<div className='poll-author poll-label'>
-					{author_label}
-					<span>
-						{poll.author}
-					</span>
-				</div>
-				<div className='poll-question poll-label'>
-					{question_label}
-					<span>
-						{poll.question}
-					</span>
-				</div>
+var PollItem = function(props) {
+	var poll = props.poll;
+	var author_label = 'Poll Author: ';
+	var question_label = 'Poll Question: ';
+	return (
+		<div className='poll well' id={'poll-' + poll.id} onClick={props.handlePollSelect.bind(null, poll.id)}>
+			<div className='poll-author poll-label'>
+				{author_label}
+				<span>
+					{poll.author}
+				</span>
 			</div>
-		);
-	}
-});
+			<div className='poll-question poll-label'>
+				{question_label}
+				<span>
+					{poll.question}
+				</span>
+			</div>
+		</div>
+	);
+};
+
+
+PollItem.propTypes = {
+	poll: ReactPropTypes.object.isRequired,
+	handlePollSelect: React.PropTypes.func.isRequired
+};
+
+
 
 module.exports = PollItem;
+
+
+
+
+// var PollItem = React.createClass({
+
+// 	propTypes: {
+// 		poll: ReactPropTypes.object.isRequired,
+// 		handlePollSelect: React.PropTypes.func.isRequired
+// 	},
+
+// 	render: function() {
+// 		var poll = this.props.poll;
+// 		var author_label = 'Poll Author: ';
+// 		var question_label = 'Poll Question: ';
+// 		return (
+// 			<div className='poll well' id={'poll-' + poll.id} onClick={this.props.handlePollSelect.bind(null, poll.id)}>
+// 				<div className='poll-author poll-label'>
+// 					{author_label}
+// 					<span>
+// 						{poll.author}
+// 					</span>
+// 				</div>
+// 				<div className='poll-question poll-label'>
+// 					{question_label}
+// 					<span>
+// 						{poll.question}
+// 					</span>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// module.exports = PollItem;
--- js/components/UserList.jsx
+++ js/components/UserList.jsx
@@ -10,51 +10,51 @@ import ReactPropTypes from 'react/lib/ReactPropTypes';
 import UserItem from './UserItem';
 import {Row, Col, Grid} from 'react-bootstrap';
 
-var UserList = React.createClass({
-
-	propTypes: {
-		allUsers: ReactPropTypes.object.isRequired
-	},
-
-	render: function() {
-		var allUsers = this.props.allUsers;
-		var userNodes = [];
-		// var users = allUsers.map(user => (<li><UserItem key={user.id} user={user}/></li>));
 
 
-		for (var key in allUsers) {
-			userNodes.push(
-				<Col key={key} xs={12} sm={6} md={3} className=''>
-					<UserItem  user={allUsers[key]} />
-				</Col>
-				);
-		}
+var UserList = function(props) {
 
-		// If there are no uses, push a div onto the empty userNodes with text 'No users.'
-		if (userNodes.length == 0) {
-			userNodes.push(
-				<Col key={1} xs={12} sm={12} md={12} className=''>
-					<div>No users.</div>
-				</Col>
+
+	var allUsers = props.allUsers;
+	var userNodes = [];
+	// var users = allUsers.map(user => (<li><UserItem key={user.id} user={user}/></li>));
+
+
+	for (var key in allUsers) {
+		userNodes.push(
+			<Col key={key} xs={12} sm={6} md={3} className=''>
+				<UserItem  user={allUsers[key]} />
+			</Col>
 			);
-		}
+	}
 
-		return (
-			<div>
-				<div id='user-list' className='poll-container-header'>
-					<h2 className='display-inline'>Listing of all Users:</h2>
-				</div>
-				<br />
-				<Grid>
-					<Row className='user-list poll-list'>
-						{userNodes}
-					</Row>
-				</Grid>
-			</div>
-
+	// If there are no uses, push a div onto the empty userNodes with text 'No users.'
+	if (userNodes.length == 0) {
+		userNodes.push(
+			<Col key={1} xs={12} sm={12} md={12} className=''>
+				<div>No users.</div>
+			</Col>
 		);
 	}
 
-});
+	return (
+		<div>
+			<div id='user-list' className='poll-container-header'>
+				<h2 className='display-inline'>Listing of all Users:</h2>
+			</div>
+			<br />
+			<Grid>
+				<Row className='user-list poll-list'>
+					{userNodes}
+				</Row>
+			</Grid>
+		</div>
+	);
+};
+
+UserList.propTypes = {
+	allUsers: ReactPropTypes.object.isRequired
+};
+
 
 module.exports = UserList;
